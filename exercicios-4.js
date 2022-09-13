dfa1ab2891f22b2f46f4cd7de9fb620476bb9a02
davi pimentel <davipimentel9@gmail.com>
2022-09-13 14:26:12 -0300

    Exercícios Javascript-5

--- exercicios-4.js
+++ exercicios-4.js
@@ -1,13 +1,15 @@
 const prompt = require('prompt-sync')();
 
-const number = parseInt(prompt('Type the number '));
+let number = parseInt(prompt('Type the number '));
 
 if(number % 2 == 0) {
 
     number = Math.sqrt(number,2)
      console.log('Par')
+     console.log(`${number}`)
 } else {
     
     number = Math.pow(number,2)
     console.log('Impar')
+    console.log(`${number}`)
 }
